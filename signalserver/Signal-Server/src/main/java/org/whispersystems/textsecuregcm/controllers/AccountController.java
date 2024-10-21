180cb41a07d580ed642732dc511424ced78ecf72
James-Lu-none <jamesabcde277@gmail.com>
2024-10-21 15:48:14 +0800

    refactor: uncomment SMS and voice verification logic in SmsSender and TwilioSmsSender

--- signalserver/Signal-Server/src/main/java/org/whispersystems/textsecuregcm/controllers/AccountController.java
+++ signalserver/Signal-Server/src/main/java/org/whispersystems/textsecuregcm/controllers/AccountController.java
@@ -136,7 +136,7 @@ public class AccountController {
     if (testDevices.containsKey(number)) {
       // noop
     } else if (transport.equals("sms")) {
-      //smsSender.deliverSmsVerification(number, client, verificationCode.getVerificationCodeDisplay());
+      smsSender.deliverSmsVerification(number, client, verificationCode.getVerificationCodeDisplay());
     } else if (transport.equals("voice")) {
       //smsSender.deliverVoxVerification(number, verificationCode.getVerificationCodeSpeech());
     }
